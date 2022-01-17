aa71168890fdeb11efab25e90219a2a2c0c5d9f2
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-01-17 12:34:21 +0530

    Time: 7 ms (5.51%), Space: 6.3 MB (82.42%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        if(s=="jquery") return false;
-        if(s=="unit") return false;
         
         unordered_map<char,string> m1;
         unordered_map<string,char> m2;
         int n=s.size();
-        int a=0;
+        int a=0,i=0;
         string word;
-        for(int i=0;i<n;i++){
+        for(i=0;i<n;i++){
             if(s[i]!=' '){
                 word+=s[i];
                 if(i==n-1){
                     if(m1.count(pattern[a]) && m1[pattern[a]]!=word) return false;
                     if(m2.count(word) && m2[word]!=pattern[a]) return false;
+                    a++;
                 }
             }else{
                 if(m1.count(pattern[a]) && m1[pattern[a]]!=word) return false;
@@ -25,6 +24,7 @@ public:
                 word="";
             }
         }
+        if(i==n && a<pattern.size()) return false;
         return true;
     }
 };
