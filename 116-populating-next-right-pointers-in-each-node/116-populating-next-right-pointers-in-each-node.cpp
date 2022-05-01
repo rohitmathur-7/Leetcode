5f1bab434b94e6d885ecea0ff1b6fb295af8c5f0
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-05-01 12:11:22 +0530

    Time: 23 ms (65.60%), Space: 16.8 MB (85.65%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -18,21 +18,16 @@ public:
 
 class Solution {
 public:
-    //BFS solution S.C - O(W) = O(N) ... W=width of binary tree, W=(n+1)/2 = O(N)  
     Node* connect(Node* root) {
         if(root==NULL) return NULL;
-        queue<Node*> q;
-        q.push(root);
-        while(!q.empty()){
-            Node* ptr=NULL;
-            int sz=q.size();
-            for(int i=0;i<sz;i++){
-                if(q.front()->right!=NULL) q.push(q.front()->right);
-                if(q.front()->left!=NULL) q.push(q.front()->left);
-                q.front()->next=ptr;
-                ptr=q.front();
-                q.pop();
+        auto L=root->left,R=root->right,N=root->next;
+        if(L!=NULL){
+            L->next=R;
+            if(N!=NULL){
+                R->next=N->left;
             }
+            connect(L);
+            connect(R);
         }
         return root;
     }
