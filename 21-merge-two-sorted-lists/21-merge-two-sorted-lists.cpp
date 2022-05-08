85e3d817954f3ff902a9fb3ab87fbed32faaa192
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-05-08 18:10:47 +0530

    Time: 9 ms (58.33%), Space: 15.1 MB (8.78%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,38 +10,36 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
-        if(list1==NULL) return list2;
-        ListNode* one=list1,*two=list2,*prev=NULL;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *ans,*one=list1,*two=list2;
+        ListNode* cur=new ListNode(0,NULL);
+        ans=cur;
         while(one!=NULL && two!=NULL){
-            if(one->val>two->val){
-                ListNode* node=new ListNode(two->val);
-                if(prev!=NULL) prev->next=node;
-                else{
-                    list1=node;
-                }
-                prev=node;
-                node->next=one;
-                two=two->next;
-            }
-            else if(one->val==two->val){
-                ListNode* node=new ListNode(two->val);
-                ListNode* nxt=one->next;
-                one->next=node;
-                node->next=nxt;
+            if(one->val<=two->val){
+                ListNode* node=new ListNode(one->val);
+                cur->next=node;
                 one=one->next;
-                two=two->next;
+                cur=cur->next;
             }
             else{
-                prev=one;
-                one=one->next;
+                ListNode* node=new ListNode(two->val);
+                cur->next=node;
+                two=two->next;
+                cur=cur->next;
             }
         }
-        while(two!=NULL){
-            prev->next=two;
-            prev=two;
-            two=two->next;
+        while(one!=NULL){
+            ListNode* node=new ListNode(one->val);
+            cur->next=node;
+            one=one->next;
+            cur=cur->next;
         }
-        return list1;
+        while(two!=NULL){
+            ListNode* node=new ListNode(two->val);
+            cur->next=node;
+            two=two->next;
+            cur=cur->next;
+        }
+        return ans->next;
     }
 };
