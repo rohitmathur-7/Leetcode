52d81a274f5848bb790aa69d1244508bc2119167
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-07-08 11:32:46 +0530

    Time: 29 ms (32.40%), Space: 26.2 MB (20.96%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int dp[102][201];
+    
+    bool solve(string s1,string s2,string s3,int idx1,int idx2,int idx3){
+        if(idx3==s3.size()){
+            if(idx1==s1.size() && idx2==s2.size()){
+                return true;
+            }
+            return false;
+        }
+        
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        
+        bool ans1=false;
+        if(s1[idx1]==s3[idx3]){
+            ans1=solve(s1,s2,s3,idx1+1,idx2,idx3+1);
+        }
+        
+        if(!ans1 && s2[idx2]==s3[idx3]){
+            ans1=solve(s1,s2,s3,idx1,idx2+1,idx3+1);
+        }
+        
+        return dp[idx1][idx2]=ans1;
+    }
+    
+    bool isInterleave(string s1, string s2, string s3) {
+        memset(dp,-1,sizeof(dp));
+        return solve(s1,s2,s3,0,0,0);
+    }
+    
+};
