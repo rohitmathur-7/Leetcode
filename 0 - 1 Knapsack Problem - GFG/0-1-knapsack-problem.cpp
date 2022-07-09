3ec7d2b8303636b26ffbe145df04aa7fc5b3f14a
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-07-09 13:14:50 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -7,24 +7,29 @@ using namespace std;
 class Solution
 {
     public:
-    int solve(int W,int wt[],int val[],int n,int idx,vector<vector<int> > &dp){
-        
-        if(idx==n || W==0) return 0;
-        
-        if(dp[idx][W]!=-1) return dp[idx][W];
-        
-        if(wt[idx]<=W){
-            return dp[idx][W]=max(val[idx]+solve(W-wt[idx],wt,val,n,idx+1,dp),solve(W,wt,val,n,idx+1,dp));
-        }
-        else return dp[idx][W]=solve(W,wt,val,n,idx+1,dp);
-    }
-    
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) { 
        // Your code here
-       vector<vector<int> > dp(n,vector<int>(W+1,-1));
-       return solve(W,wt,val,n,0,dp);
+       vector<vector<int> > dp(n+1,vector<int>(W+1));
+       //initialization
+       for(int i=0;i<=n;i++){
+           dp[i][0]=0;
+       }
+       for(int i=0;i<=W;i++){
+           dp[0][i]=0;
+       }
        
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=W;j++){
+               if(wt[i-1]<=j){
+                   dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+               }
+               else dp[i][j]=dp[i-1][j];
+           }
+       }
+       
+       return dp[n][W];
+         
     }
 };
 
