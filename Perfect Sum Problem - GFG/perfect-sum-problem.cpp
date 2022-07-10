84faca33a2547904ef3a81ab39c603c11ab33c18
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-07-10 19:16:31 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -7,25 +7,21 @@ class Solution{
 
 	public:
 	int mod=1000000007;
-	int solve(int arr[],int n,int sum,int idx,int cur,vector<vector<int> > &dp){
-	    if(cur>sum) return 0;
-	    if(idx==n){
-	        if(cur==sum) return 1;
-	        return 0;
-	    }
+	
+	int solve(int arr[], int n, int sum,vector<vector<int> > &dp){
+	    if(sum==0 && n==0) return 1;
+	    if(sum<0 || n<0) return 0;
 	    
-	    if(dp[idx][cur]!=-1){
-	       // cout<<"idx: "<<idx<<" cur: "<<cur<<endl;
-	        return dp[idx][cur]%mod;
-	    }
+	    if(dp[n][sum]!=-1) return dp[n][sum]%mod;
 	    
-	    return dp[idx][cur]=((solve(arr,n,sum,idx+1,cur,dp))%mod+(solve(arr,n,sum,idx+1,cur+arr[idx],dp))%mod)%mod;
+	    return dp[n][sum]=(solve(arr,n-1,sum,dp)%mod+solve(arr,n-1,sum-arr[n-1],dp)%mod)%mod;
+	    
 	}
 	
-	int perfectSum(int arr[],int n,int sum){
+	int perfectSum(int arr[], int n, int sum){
         // Your code goes here
         vector<vector<int> > dp(n+1,vector<int>(sum+1,-1));
-        return solve(arr,n,sum,0,0,dp)%mod;
+        return solve(arr,n,sum,dp)%mod;
 	}
 	  
 };
