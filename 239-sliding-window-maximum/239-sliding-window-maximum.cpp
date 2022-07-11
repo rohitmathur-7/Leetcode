dbe5ed8737e8a13fdc6889bfa2fe85f65f3b5569
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-07-11 11:46:47 +0530

    Time: 1161 ms (5.01%), Space: 158.3 MB (19.68%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k){
-        int n=nums.size();
-        deque<int> dq;
-        vector<int> ans;
-        for(int i=0;i<n;i++){
-            while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
-            while(!dq.empty() && nums[dq.back()]<=nums[i]) dq.pop_back();
-            dq.push_back(i);
-            if(i>=k-1) ans.push_back(nums[dq.front()]);
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        int n=arr.size();
+        priority_queue<int> pq;
+        map<int,int> mp;
+        for(int i=0;i<k;i++){
+            pq.push(arr[i]);
+            mp[arr[i]]++;
         }
+        
+        vector<int> ans;
+        ans.push_back(pq.top());
+        int c=0;
+        for(int i=k;i<n;i++){
+            mp[arr[c]]--;
+            mp[arr[i]]++;
+            pq.push(arr[i]);
+            // cout<<"c: "</<c<<" arrc: "<<arr[c]<<" tp: "<<pq.top()<<endl;
+            if(pq.top()==arr[c]) pq.pop();
+            // cout<<"top: "<<pq.top()<<endl;
+            while(mp[pq.top()]==0){
+                pq.pop();
+            }
+            ans.push_back(pq.top());
+            c++;
+        }
+        
         return ans;
     }
 };
