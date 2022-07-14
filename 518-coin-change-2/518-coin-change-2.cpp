c7640f59c097e500a394dbfda32260c534ad418b
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-07-14 07:54:19 +0530

    Time: 35 ms (53.32%), Space: 11.9 MB (69.10%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -11,7 +11,7 @@ public:
         }
         
         for(int i=1;i<=n;i++){
-            for(int j=0;j<=amount;j++){
+            for(int j=1;j<=amount;j++){
                 if(coins[i-1]<=j){
                     dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
                 }
