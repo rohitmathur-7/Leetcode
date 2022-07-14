dcbc104b48230404e6ea4135f81687232c933624
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-07-14 11:58:26 +0530

    Time: 211 ms (19.01%), Space: 50.2 MB (7.47%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     
-    int findLowestCoins(vector<int> &coins, int cur, int amount,vector<vector<int> > &dp){  
-        if (cur == coins.size() || amount <= 0)
-            return (amount == 0) ? 0 : INT_MAX - 1;   
+    int solve(vector<int> &coins,int cur,int amount,vector<vector<int> > &dp){
+        if(cur==coins.size() || amount<=0)
+            return (amount==0)?0:INT_MAX-1;
         
-        if (dp[cur][amount] != -1)
+        if(dp[cur][amount] != -1) 
             return dp[cur][amount];
         
-        if (coins[cur] > amount) {
-            return dp[cur][amount]=findLowestCoins(coins,cur+1,amount,dp);
+        if(coins[cur]>amount){
+            return dp[cur][amount]=solve(coins,cur+1,amount,dp);
         }
-        else {
-            return dp[cur][amount]=min(findLowestCoins(coins,cur+1,amount,dp),1 + findLowestCoins(coins,cur,amount-coins[cur],dp));
+        else{
+            return dp[cur][amount]=min(solve(coins,cur+1,amount,dp),1+solve(coins,cur,amount-coins[cur],dp));
         }
+        
     }
     
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& coins, int amount){
         int n=coins.size();
         vector<vector<int> > dp(n+1,vector<int>(amount+1,-1));
-        int res = findLowestCoins(coins,0,amount,dp);
-        return (res == INT_MAX - 1 ) ? -1 : res;
+        
+        int ans=solve(coins,0,amount,dp);
+        return (ans==INT_MAX-1)?-1:ans;
     }
 };
