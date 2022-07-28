bb0d9ced0bb23b9d3413690c456fae6151377ccf
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-07-28 07:57:33 +0530

    Time: 52 ms (55.13%), Space: 12.6 MB (40.65%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& arr) {
-        int n=arr.size();
-        int m=arr[0].size();
-        queue<pair<int,int>> q;
+    
+    void dfs(vector<vector<char>>& grid,vector<vector<bool> > &vis,int i,int j,int &n,int &m){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]=='0' || vis[i][j]) return;
+        
+        vis[i][j]=true;
+        dfs(grid,vis,i-1,j,n,m);
+        dfs(grid,vis,i+1,j,n,m);
+        dfs(grid,vis,i,j-1,n,m);
+        dfs(grid,vis,i,j+1,n,m);
+    }
+    
+    int numIslands(vector<vector<char>>& grid){
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool> > vis(n,vector<bool>(m,false));
+        
         int ans=0;
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(arr[i][j]=='1'){
+                if(!vis[i][j] && grid[i][j]=='1'){
                     ans++;
-                    q.push({i,j});
-                    while(!q.empty()){
-                        int x=q.front().first;
-                        int y=q.front().second;
-                        arr[x][y]='0';
-                        q.pop();
-                        if((x+1)<n && arr[x+1][y]=='1'){
-                            arr[x+1][y]='0';
-                            q.push({x+1,y});
-                        }
-                        if((x-1)>=0 && arr[x-1][y]=='1'){
-                            arr[x-1][y]='0';
-                            q.push({x-1,y});
-                        }
-                        if((y+1)<m && arr[x][y+1]=='1'){
-                            arr[x][y+1]='0';
-                            q.push({x,y+1});
-                        }
-                        if((y-1)>=0 && arr[x][y-1]=='1'){
-                            arr[x][y-1]='0';
-                            q.push({x,y-1});
-                        }
-                    }
+                    dfs(grid,vis,i,j,n,m);
                 }
             }
         }
+        
         return ans;
     }
 };
