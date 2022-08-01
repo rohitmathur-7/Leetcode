c8b417a6949b46875024e56c1b63fcd673ae38bb
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-08-01 11:32:45 +0530

    Time: 604 ms (100.00%), Space: 153.2 MB (100.00%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,35 +1,54 @@
 class Solution {
 public:
     
-    void bfs(int node,vector<int> &edges,vector<int> &dist){
-        dist[node]=0;
-        queue<int> q;
-        q.push(node);
-        
-        while(!q.empty()){
-            int a=q.front();
-            q.pop();
-            if(edges[a]!=-1 && dist[edges[a]]==-1){
-                dist[edges[a]]=dist[a]+1;
-                q.push(edges[a]);
-            }
+    void dfs(vector<int> &adj,vector<bool> &vis,int i,vector<pair<int,int>> &a,int c){
+        a.push_back({i,c});
+        vis[i]=true;
+        if(adj[i]!=-1 && !vis[adj[i]]){
+            dfs(adj,vis,adj[i],a,c+1);
         }
-        
-        
+    }
+    void dfs2(vector<int> &adj,vector<bool> &vis,int i,vector<pair<int,int>> &a,int c,unordered_map<int,int> &mp){
+        a.push_back({i,c});
+        // fs.push_back(i);
+        mp[i]=c;
+        vis[i]=true;
+        if(adj[i]!=-1 && !vis[adj[i]]){
+            dfs2(adj,vis,adj[i],a,c+1,mp);
+        }
     }
     
-    int closestMeetingNode(vector<int>& edges, int node1, int node2){
-        int n=edges.size();
-        vector<int> a(n,-1),b(n,-1);
+    int closestMeetingNode(vector<int>& adj, int node1, int node2) {
+        vector<pair<int,int>> a,b;
+        vector<int> fs;
+        int V=adj.size();
+     
+        vector<bool> vis(V,false);
         
-        bfs(node1,edges,a);
-        bfs(node2,edges,b);
-        
-        int mn=INT_MAX,ans=-1;
-        for(int i=0;i<n;i++){
-            if(min(a[i],b[i])>=0 && max(a[i],b[i])<mn){
-                mn=max(a[i],b[i]);
-                ans=i;
+        int c=0;
+        dfs(adj,vis,node1,a,c);
+        vis.assign(V,false);
+        c=0;
+        unordered_map<int,int> mp;
+        dfs2(adj,vis,node2,b,c,mp);
+
+        int mn=INT_MAX,d1,d2,d,ans=-1;            
+        for(auto x:a){
+            int pos;
+            if(mp.count(x.first)){
+                // pos=find(fs.begin(),fs.end(),x.first)-fs.begin();
+                pos=mp[x.first];
+                d1=x.second;
+                d2=b[pos].second;
+                d=max(d1,d2);
+                //cout<<"xf: "<<x.first<<" xs: "<<x.second<<" "<<b[pos].second<<endl;               
+                
+                if(d<=mn){
+                    if(ans==-1) ans=x.first;
+                    else if(mn==d) ans=min(ans,x.first);
+                    else ans=x.first;
+                    mn=d;
+                }
             }
         }
         
