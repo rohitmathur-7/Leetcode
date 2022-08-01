e53d2d1b204a088b8df9f444c2deab989122941c
rohitmathur-7 <62138457+rohitmathur-7@users.noreply.github.com>
2022-08-01 11:21:23 +0530

    Time: 270 ms (100.00%), Space: 100.5 MB (100.00%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     
-    void dfs(int node,vector<int> &edges,int c,vector<int> &dist){
-        while(node!=-1 && dist[node]==-1){
-            dist[node]=c++;
-            node=edges[node];
+    void bfs(int node,vector<int> &edges,vector<int> &dist){
+        dist[node]=0;
+        queue<int> q;
+        q.push(node);
+        
+        while(!q.empty()){
+            int a=q.front();
+            q.pop();
+            if(edges[a]!=-1 && dist[edges[a]]==-1){
+                dist[edges[a]]=dist[a]+1;
+                q.push(edges[a]);
+            }
         }
+        
+        
     }
     
     int closestMeetingNode(vector<int>& edges, int node1, int node2){
         int n=edges.size();
         vector<int> a(n,-1),b(n,-1);
         
-        dfs(node1,edges,0,a);
-        dfs(node2,edges,0,b);
+        bfs(node1,edges,a);
+        bfs(node2,edges,b);
         
         int mn=INT_MAX,ans=-1;
         for(int i=0;i<n;i++){
@@ -24,6 +34,5 @@ public:
         }
         
         return ans;
-        
     }
 };
